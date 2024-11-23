0ea8880e1f63a271aa057d85ba5c9fca86aabfc9
Victor Machado <victormachadogp@gmail.com>
2024-11-22 22:43:25 -0300

    Adiciona input dinâmico - gorjeta e número pessoas
    - Adiciona lógica de input dinâmica do tipo intervalo (range) para permitir ao usuário ajustar a porcentagem da gorjeta e o número de pessoas.
    
    - Implementa um array reativo `rangeInputs` para armazenar os valores e limites de cada input, além de uma função `updateValue` para atualizar o array quando os valores são alterados

--- src/components/RangeInput.vue
+++ src/components/RangeInput.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>Input range</div>
+  <div class="input-range">
+    <label :for="id" class="input-range__label">{{ label }}</label>
+    <input
+      type="range"
+      :id="id"
+      :min="min"
+      :max="max"
+      :value="value"
+      @input="$emit('update', +$event.target.value)"
+    />
+    <span class="input-range__value">{{ value }}</span>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  label: String,
+  value: {
+    type: Number,
+    required: true,
+  },
+  min: {
+    type: Number,
+    default: 0,
+  },
+  max: {
+    type: Number,
+    default: 100,
+  },
+  id: String,
+})
 
-<style></style>
+defineEmits(['update'])
+</script>
+
+<style scoped>
+.input-range {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.input-range__slider {
+  width: 100%;
+}
+
+.input-range__value {
+  font-weight: bold;
+}
+</style>
--- src/components/TipCalculator.vue
+++ src/components/TipCalculator.vue
@@ -17,7 +17,15 @@
       </label>
     </div>
 
-    <RangeInput />
+    <RangeInput
+      v-for="(input, index) in rangeInputs"
+      :key="index"
+      :label="input.label"
+      :value="input.value"
+      :min="input.min"
+      :max="input.max"
+      @update="(newValue) => updateValue(index, newValue)"
+    />
 
     <ResultsPanel />
   </div>
@@ -31,7 +39,19 @@ import RangeInput from './RangeInput.vue'
 import ResultsPanel from './ResultsPanel.vue'
 
 const billTotal = ref(0)
+const tipPercentage = ref(10)
+const peopleCount = ref(2)
 const selectedCurrency = ref('USD')
+
+// Dados dinâmicos
+const rangeInputs = ref([
+  { label: 'Tip', value: 10, min: 10, max: 20 },
+  { label: 'Number of People', value: 2, min: 2, max: 16 },
+])
+
+const updateValue = (index, newValue) => {
+  rangeInputs.value[index].value = newValue
+}
 </script>
 
 <style></style>
