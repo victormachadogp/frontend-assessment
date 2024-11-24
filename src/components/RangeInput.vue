641391ba3866046808d2652589dd21a0bb0a75fa
Victor Machado <victormachadogp@gmail.com>
2024-11-23 23:31:31 -0300

    Estilizando rangeInput

--- src/components/RangeInput.vue
+++ src/components/RangeInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input-range">
-    <label :for="inputId" class="input-range__label"> {{ label }} ({{ modelValue }}) </label>
+    <label class="input-range__label"> {{ label }} ({{ modelValue }}) </label>
 
     <div class="input-range__container">
       <input
@@ -52,18 +52,108 @@ const handleInput = (event) => {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .input-range {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
+  $primary-color: #820be0;
+  $secondary-color: #f1f1f1;
+  $track-color: #ddd;
 
-.input-range__slider {
-  width: 100%;
-}
+  margin: 1.5rem 2rem;
 
-.input-range__value {
-  font-weight: bold;
+  &__label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    color: #333;
+  }
+
+  &__container {
+    position: relative;
+  }
+
+  &__slider {
+    width: 100%;
+    height: 6px;
+    -webkit-appearance: none;
+    appearance: none;
+    background: $track-color;
+    outline: none;
+    border-radius: 3px;
+
+    margin: 1rem 0;
+
+    &::-webkit-slider-runnable-track {
+      -webkit-appearance: none;
+      color: #13bba4;
+      margin-top: -1px;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 20px;
+      height: 20px;
+      background: $primary-color;
+      border-radius: 50%;
+      cursor: pointer;
+      transition: background-color 0.2s ease;
+
+      &:hover {
+        background: darken($primary-color, 10%);
+      }
+
+      &:active {
+        transform: scale(1.1);
+      }
+    }
+
+    &::-moz-range-thumb {
+      width: 20px;
+      height: 20px;
+      background: $primary-color;
+      border: none;
+      border-radius: 50%;
+      cursor: pointer;
+      transition: background-color 0.2s ease;
+
+      &:hover {
+        background: darken($primary-color, 10%);
+      }
+
+      &:active {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  &__values {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 0.25rem;
+    font-size: 0.875rem;
+    color: #666;
+  }
+
+  &__min,
+  &__max {
+    position: relative;
+    top: -0.5rem;
+  }
+
+  @media (max-width: 768px) {
+    &__slider {
+      height: 8px;
+
+      &::-webkit-slider-thumb {
+        width: 24px;
+        height: 24px;
+      }
+
+      &::-moz-range-thumb {
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
 }
 </style>
