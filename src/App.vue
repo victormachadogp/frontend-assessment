61a7a2556db7becb62364703b071910019f01b44
Victor Machado <victormachadogp@gmail.com>
2024-11-25 20:54:38 -0300

    Adicionado titulo Le Tip

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import TipCalculator from './components/TipCalculator.vue'
 </script>
 
 <template>
+  <h1>Le Tip</h1>
   <TipCalculator />
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin-bottom: 1rem;
+  font-weight: 700;
+}
+</style>
